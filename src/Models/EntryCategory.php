82b3eb07d739dd37e52bd010b676e9a447004098
yannelli <59575788+yannelli@users.noreply.github.com>
2025-12-26 06:27:50 +0000

    Fix styling

--- src/Models/EntryCategory.php
+++ src/Models/EntryCategory.php
@@ -3,7 +3,6 @@
 namespace Yannelli\EntryVault\Models;
 
 use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
